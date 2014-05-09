71d25184941d2517763a5ddcb89815169590c666
Mallesh Koujalagi <mallesh.koujalagi.sandiego@gmail.com>
2014-05-08 23:22:49 -0700

    Code changed for reading all snipper data from active server and print based on service
    Signed-off-by: Mallesh Koujalagi <mallesh.koujalagi.sandiego@gmail.com>

--- port-snoop.c
+++ port-snoop.c
@@ -5,6 +5,30 @@
 
 int main(int argc, char **agrv)
 {
+ int max=3000, s,i,ret;
+ struct sockaddr_in serv;
+
+ s=socket(AF_INET, SOCK_STRAM, 0)
+ if(s < 0)
+ {
+  printf("Socket failed \n");
+  return -1;
+ }
+ 
+ serv.sin_addr.s_add = inet_addr(argv[1]);
+ serv.sin_family = AF_INET
+
+ for(i = 0; i < max; i++)
+ {
+  serv.sin_port = htons(i);
+  ret = connect(s, (struct sockaddr *)&serv,sizeof(serv));
+  if(ret == 0)
+  {
+   printf("Service on %d\n", i);
+  }
+
+ }
+ close(s);
  printf("Helow World\n");
  return 0;
 }
